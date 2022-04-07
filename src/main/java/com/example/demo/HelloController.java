b8ef340f5a3a18c74ce79b37a17e491cea4e19e4
abobus <lyahovss@mer.ci.nsu.ru>
2022-04-08 04:44:16 +0700

    Initial commit

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,55 @@
+package com.example.demo;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+import java.io.*;
+
+public class HelloController {
+    @FXML
+    private TableView tabletka;
+    @FXML
+    private TextField fileName;
+
+    public static class stroka {
+        private final SimpleStringProperty id;
+        private final SimpleStringProperty value;
+
+        private stroka(String fName, String lName) {
+            this.id = new SimpleStringProperty(fName);
+            this.value = new SimpleStringProperty(lName);
+        }
+
+        public stroka(SimpleStringProperty id, SimpleStringProperty value) {
+            this.id = id;
+            this.value = value;
+        }
+    }
+    @FXML
+    protected void Zapolnenie() {
+        //ArrayList<String> arrayList = new ArrayList<>();
+        try {
+            File file = new File(fileName.getText());
+            //создаем объект FileReader для объекта File
+            FileReader fr = new FileReader(file);
+            //создаем BufferedReader с существующего FileReader для построчного считывания
+            BufferedReader reader = new BufferedReader(fr);
+            // считаем сначала первую строку
+            String line = reader.readLine();
+            int i = 0;
+            String j = "0";
+            while (line != null) {
+                System.out.println(line);
+                j = Integer.toString(i);
+                tabletka.getItems().add(i, new stroka(j,line));
+                // считываем остальные строки в цикле
+                line = reader.readLine();
+                i++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
